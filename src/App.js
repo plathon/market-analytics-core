be0669d165a823b6d445c79eb104378cb603c049
Renan Plathon <rplathon@gmail.com>
2020-03-19 21:44:52 -0300

    router config with react-router lib

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./App.css";
 
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signin">signin</Link>
+            </li>
+            <li>
+              <Link to="/signup">signup</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/signin">
+            <Signin />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
