19e88c8d08151f47c6e990874c0eaa27641b740a
Renan Plathon <rplathon@gmail.com>
2020-03-20 22:30:59 -0300

    signup page and form setup

--- src/components/signup/SignupForm.js
+++ src/components/signup/SignupForm.js
@@ -1,208 +1,150 @@
-import React, { Component } from "react";
-
+import React from "react";
 import { Form, Input, Select, Checkbox, Button } from "antd";
 
 const { Option } = Select;
 
-class NormalSignupForm extends Component {
-  state = {
-    confirmDirty: false
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFieldsAndScroll((err, data) => {
-      if (!err) this.props.signup(data);
-    });
-  };
-
-  handleConfirmBlur = e => {
-    const value = e.target.value;
-    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-  };
-
-  compareToFirstPassword = (rule, value, callback) => {
-    const form = this.props.form;
-    if (value && value !== form.getFieldValue("password")) {
-      callback("Two passwords that you enter is inconsistent!");
-    } else {
-      callback();
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 }
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 }
+  }
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0
+    },
+    sm: {
+      span: 16,
+      offset: 8
     }
+  }
+};
+
+const RegistrationForm = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = values => {
+    console.log("Received values of form: ", values);
   };
 
-  validateToNextPassword = (rule, value, callback) => {
-    const form = this.props.form;
-    if (value && this.state.confirmDirty) {
-      form.validateFields(["confirm"], { force: true });
-    }
-    callback();
-  };
-
-  validateAgreeTerms = (rule, value, callback) => {
-    if (!value) {
-      callback("You Most accept the terms!");
-    }
-    callback();
-  };
-
-  render() {
-    const { getFieldDecorator } = this.props.form;
-
-    const formItemLayout = {
-      labelCol: {
-        xs: { span: 24 },
-        sm: { span: 8 }
-      },
-      wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 16 }
-      }
-    };
-
-    const tailFormItemLayout = {
-      wrapperCol: {
-        xs: {
-          span: 24,
-          offset: 0
-        },
-        sm: {
-          span: 16,
-          offset: 8
-        }
-      }
-    };
-
-    const prefixSelector = getFieldDecorator("prefix", {
-      initialValue: "55"
-    })(
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
       <Select style={{ width: 70 }}>
         <Option value="55">+55</Option>
       </Select>
-    );
+    </Form.Item>
+  );
 
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Item {...formItemLayout} label="Name">
-          {getFieldDecorator("name", {
-            rules: [
-              {
-                required: true,
-                message: "Please input your Name!"
+  return (
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      initialValues={{
+        prefix: "55"
+      }}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="name"
+        label="Name"
+        rules={[
+          {
+            required: true,
+            message: "Please input your E-mail!"
+          }
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: "email",
+            message: "The input is not valid E-mail!"
+          },
+          {
+            required: true,
+            message: "Please input your E-mail!"
+          }
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!"
+          }
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={["password"]}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please confirm your password!"
+          },
+          ({ getFieldValue }) => ({
+            validator(rule, value) {
+              if (!value || getFieldValue("password") === value) {
+                return Promise.resolve();
               }
-            ]
-          })(<Input />)}
-        </Form.Item>
+              return Promise.reject(
+                "The two passwords that you entered do not match!"
+              );
+            }
+          })
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
 
-        <Form.Item {...formItemLayout} label="E-mail">
-          {getFieldDecorator("email", {
-            rules: [
-              {
-                type: "email",
-                message: "The input is not valid E-mail!"
-              },
-              {
-                required: true,
-                message: "Please input your E-mail!"
-              }
-            ]
-          })(<Input />)}
-        </Form.Item>
+      <Form.Item
+        name="phone"
+        label="Phone Number"
+        rules={[{ required: true, message: "Please input your phone number!" }]}
+      >
+        <Input addonBefore={prefixSelector} style={{ width: "100%" }} />
+      </Form.Item>
 
-        <Form.Item {...formItemLayout} label="CPF">
-          {getFieldDecorator("cpf", {
-            rules: [
-              {
-                required: true,
-                message: "Please input your CPF!"
-              }
-            ]
-          })(<Input />)}
-        </Form.Item>
+      <Form.Item
+        name="agreement"
+        valuePropName="checked"
+        {...tailFormItemLayout}
+      >
+        <Checkbox>
+          I have read the <a href="">agreement</a>
+        </Checkbox>
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
-        <Form.Item {...formItemLayout} label="Password">
-          {getFieldDecorator("password", {
-            rules: [
-              {
-                required: true,
-                message: "Please input your password!"
-              },
-              {
-                validator: this.validateToNextPassword
-              }
-            ]
-          })(<Input type="password" />)}
-        </Form.Item>
-
-        <Form.Item {...formItemLayout} label="Confirm Password">
-          {getFieldDecorator("confirm", {
-            rules: [
-              {
-                required: true,
-                message: "Please confirm your password!"
-              },
-              {
-                validator: this.compareToFirstPassword
-              }
-            ]
-          })(<Input type="password" onBlur={this.handleConfirmBlur} />)}
-        </Form.Item>
-
-        <Form.Item {...formItemLayout} label="Phone Number (Whatsapp)">
-          {getFieldDecorator("phone", {
-            rules: [
-              { required: true, message: "Please input your phone number!" }
-            ]
-          })(<Input addonBefore={prefixSelector} style={{ width: "100%" }} />)}
-        </Form.Item>
-
-        <Form.Item {...formItemLayout} label="State">
-          {getFieldDecorator("state", {
-            rules: [
-              {
-                required: true,
-                message: "Please input your state!"
-              }
-            ]
-          })(<Input />)}
-        </Form.Item>
-
-        <Form.Item {...formItemLayout} label="City">
-          {getFieldDecorator("city", {
-            rules: [
-              {
-                required: true,
-                message: "Please input your city!"
-              }
-            ]
-          })(<Input />)}
-        </Form.Item>
-
-        <Form.Item {...tailFormItemLayout}>
-          {getFieldDecorator("agreement", {
-            valuePropName: "checked",
-            rules: [{ validator: this.validateAgreeTerms }]
-          })(
-            <Checkbox>
-              I have read the <span>agreement</span>
-            </Checkbox>
-          )}
-        </Form.Item>
-
-        <Form.Item {...tailFormItemLayout}>
-          <Button
-            type="primary"
-            htmlType="submit"
-            loading={this.props.isLoading}
-          >
-            Register
-          </Button>
-        </Form.Item>
-      </Form>
-    );
-  }
-}
-
-const WrappedNormalSignupForm = Form.create({ name: "normal_signup_form" })(
-  NormalSignupForm
-);
-export default WrappedNormalSignupForm;
+export default RegistrationForm;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import { Row, Col } from "antd";
+
+import SignupFor from "../components/signup/SignupForm";
+
 function Signup() {
-  return <h1>signup</h1>;
+  return (
+    <Row>
+      <Col xs={2} sm={4} md={6} lg={6} xl={6}></Col>
+
+      <Col xs={20} sm={16} md={12} lg={10} xl={10} style={{ marginTop: 100 }}>
+        <SignupFor />
+      </Col>
+    </Row>
+  );
 }
 
 export default Signup;
